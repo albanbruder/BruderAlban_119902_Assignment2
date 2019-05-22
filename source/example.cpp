272f49f34cc06be2f9a7345dc6757ef82aac95d3
Alban Bruder <mail@albanbruder.de>
2019-05-22 18:29:16 +0200

    Add comments to example.cpp

--- source/example.cpp
+++ source/example.cpp
@@ -6,27 +6,33 @@
 int main(int argc, char *argv[]) {
   std::vector<float> numbers;
 
+  // get numbers from arguments (if there are any)
   if(argc > 1) {
     for (int i=1; i<argc; ++i) {
       numbers.push_back(std::atof(argv[i]));
     }
+
+  // or wait for user input
   } else {
-    // read input from cin
+    // read one line from std::cin
     std::string input;
     std::getline(std::cin, input);
+
+    // parse floats from string
     std::istringstream inputStream(input);
-    
     std::copy(std::istream_iterator<float>(inputStream),
       std::istream_iterator<float>(),
       std::back_inserter(numbers));
   }
 
+  // sort with heapsort.hpp
   buw::heapsort(
     std::begin(numbers), 
     std::end(numbers), 
     [](float l, float r) { return l < r; }
   );
 
+  // output sorted vector
   std::copy(std::cbegin(numbers), std::cend(numbers), std::ostream_iterator<float>(std::cout, " "));
   return 0;
 }
