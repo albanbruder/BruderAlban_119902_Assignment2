c1d0aa6cef53a973fda0c188c017c30685c6a4bb
Alban Bruder <mail@albanbruder.de>
2019-05-23 16:33:36 +0200

    Parse arguments

--- source/example.cpp
+++ source/example.cpp
@@ -3,17 +3,49 @@
 #include <iostream>
 #include <sstream>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int main(int argc, char *argv[]) {
+  bool help_flag = false;
+  bool quiet_flag = false;
+
+  // parse arguments
+  char c;
+  while((c = getopt(argc, argv, "hq")) != -1) {
+    switch(c) {
+      case 'h':
+        help_flag = true;
+        break;
+      case 'q':
+        quiet_flag = true;
+        break;
+      default:
+        abort();
+    }
+  }
+
+  // if the user asked for help: output help message
+  if(help_flag) {
+    std::cout << "Usage: " << argv[0] << " [-h] [-q] [NUMBER NUMBER ...]" << "\n";
+    return 0;
+  }
+
   std::vector<float> numbers;
 
-  // get numbers from arguments (if there are any)
-  if(argc > 1) {
-    for (int i=1; i<argc; ++i) {
+  // get numbers from arguments
+  for (int i=1; i<argc; ++i) {
+    if(argv[i][0] != '-') {
       numbers.push_back(std::atof(argv[i]));
     }
+  }
 
-  // or wait for user input
-  } else {
+  // if no numbers where passed: prompt the user
+  if(numbers.size() == 0) {
+    if(!quiet_flag) {
+      std::cout << "Please the numbers you want to sort separated with spaces:" << "\n";
+    }
     // read one line from std::cin
     std::string input;
     std::getline(std::cin, input);
